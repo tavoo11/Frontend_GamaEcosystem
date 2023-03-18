f411ab1295fc40db5aa634ca569e7391df8bac90
tavoo11 <gustavobuelvasm@gmail.com>
2023-03-18 13:59:43 -0500

    man city

---
+++ src/componentes/Chat.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../assetss/css/mensajeCss.css';
+import axios from "axios";
+
+function Chat(props){
+   const {usuarioChat, conversaciones, userId} = props;
+    return(
+        
+        <div>
+
+        </div>
+    )
+}
+export default Chat;
--- src/componentes/Mensaje.jsx
+++ src/componentes/Mensaje.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import '../assetss/css/mensajeCss.css';
 import jwt_decode from 'jwt-decode';
 import Axios from "axios";
+import Chat from "./Chat";
 import {
     MDBContainer,
     MDBRow,
@@ -15,78 +16,100 @@ import {
     MDBCardHeader,
   } from "mdb-react-ui-kit";
 
-function Mensaje(){
-    const [mensajes, setMensajes] = useState([]);
-    const [form, setForm] = useState({
-      nombre: '',
-      apellidos: '',
-      perfil:{
-        imagen: ''
-      }
-    });
+  function Mensaje() {
+    const [mensajes, setMensajes] = useState({});
+    const [conversaciones, setConversaciones] = useState([]);
+    const [usuarioChat, setUsuarioChat] = useState(null);
+    const [form, setForm] = useState({});
     const [usuarios, setUsuarios] = useState([]);
- //captura lo que se escriba en el input y lo guarda en mensajes
-    function ManejadorInput(event){
-      const {name, value} = event.target;
-      setMensajes (prevState => ({
+  
+    const token = localStorage.getItem("token");
+    const pk = parseInt(jwt_decode(token).user_id, 10);
+  
+    function manejarInput(event) {
+      const { name, value } = event.target;
+      setMensajes((prevState) => ({
         ...prevState,
-        [name]: value
+        [name]: value,
       }));
     }
-    //enviar mensaje
-    function enviarMensaje(){
-      mensajes = '';
+  
+    function enviarMensaje(event) {
+      event.preventDefault();
+      const data = {
+        contenido: mensajes.mensaje,
+        id_emisor: pk,
+        id_receptor: usuarioChat.id,
+      };
+      Axios.post("http://127.0.0.1:8000/mensaje/", data)
+        .then((response) => {
+          setConversaciones([...conversaciones, response.data]);
+          setMensajes({});
+        })
+        .catch((error) => console.log(data));
     }
-    const [mostrarMensaje, setMostrarMensaje] = useState(true)
-
-    /* function CerrarChat(){
-      setMostrarMensaje(false)
-    } */
-    useEffect(() =>{
-     const token = localStorage.getItem("token");
-      if (token){
-        let pk = jwt_decode(token).user_id.toString();
-        console.log(pk);
-        if (token) {
-            // Configurar cabecera de la solicitud
-            const config = {
-                headers: {
-                    "Authorization": `Token ${token}`
-                }
-            };
-
-            // Realizar solicitud GET a la API para obtener el perfil del usuario
-            Axios.get(`http://127.0.0.1:8000/usuarioid/${pk}`, config)
-                .then(response => {
-                    const data = response.data;
-                    ListaUsuarios();
-                    setForm({
-                        
-                        username: data.username,
-                        nombre: data.nombre,
-                        apellidos: data.apellidos,
-                        perfil: {
-                            imagen: data.perfil.imagen
-                        }
-                    });
-                })
-                .catch(error => console.log(error));
-
-          //lista de usuario
-          
-        async function ListaUsuarios() {
-          await Axios.get('http://127.0.0.1:8000/usuarioid/', config).then(res => {
-              const data = res.data;
-              setUsuarios(data);
-            })
-         }
-          
-        }
+  
+    useEffect(() => {
+      obtenerPerfilUsuario();
+      obtenerConversaciones();
+      obtenerListaUsuarios();
+    }, [usuarioChat]);
+  
+    async function obtenerPerfilUsuario() {
+      try {
+        const config = { headers: { Authorization: `Token ${token}` } };
+        const response = await Axios.get(
+          `http://127.0.0.1:8000/usuarioid/${pk}`,
+          config
+        );
+        const data = response.data;
+        setForm({
+          username: data.username,
+          nombre: data.nombre,
+          apellidos: data.apellidos,
+          perfil: {
+            imagen: data.perfil.imagen,
+          },
+        });
+      } catch (error) {
+        console.log(error);
       }
-    }, []);
-
+    }
+  
+    async function obtenerConversaciones() {
+      try {
+        if (!usuarioChat) {
+          return;
+        }
+        const userId = jwt_decode(token).user_id;
+        const config = {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        };
+        const response = await Axios.get(
+          `http://127.0.0.1:8000/mensaje/${userId}/${usuarioChat.id}`,
+          config
+        );
+        setConversaciones(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  
+    async function obtenerListaUsuarios() {
+      try {
+        const config = { headers: { Authorization: `Token ${token}` } };
+        const response = await Axios.get("http://127.0.0.1:8000/usuarioid/", config);
+        setUsuarios(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
     return (
             <MDBContainer fluid className="py-5 gradient-custom">
+              <Chat usuarioChat={usuarioChat} conversaciones={conversaciones} userId={pk}/>
               <MDBRow>
                 <MDBCol md="6" lg="5" xl="4" className="mb-4 mb-md-0">
                   <h5 className="font-weight-bold mb-3 text-center text-white">
@@ -104,7 +127,7 @@ function Mensaje(){
                             borderBottom: "1px solid rgba(255,255,255,.3) !important",
                           }}
                         >
-                          <a style={{textDecoration: 'none'}}
+                          <a onClick={()=> setUsuarioChat(user)} style={{textDecoration: 'none'}}
                             href="#!"
                             className="d-flex justify-content-between link-light"
                           >
@@ -117,7 +140,7 @@ function Mensaje(){
                                 height="60"
                               />
                               <div className="pt-1">
-                                <p className="fw-bold mb-0">{user.nombre} {user.apellidos}</p>
+                                <p className="fw-bold mb-0" >{user.nombre} {user.apellidos}</p>
                                 <p className="small text-white">
                                   {user.perfil.descripcion}
                                 </p>
@@ -142,84 +165,50 @@ function Mensaje(){
                     </MDBCardBody>
                   </MDBCard>
                 </MDBCol>
-        
-                <MDBCol md="6" lg="7" xl="8" className="mask-custom">
-                  <MDBTypography listUnStyled className="text-white">
-                    <li className="d-flex justify-content-between mb-4">
-                      <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp"
-                        alt="avatar"
-                        className="rounded-circle d-flex align-self-start me-3 shadow-1-strong"
-                        width="60"
-                      />
-                      <MDBCard className="mask-custom">
-                        <MDBCardHeader
-                          className="d-flex justify-content-between p-3"
-                          style={{ borderBottom: "1px solid rgba(255,255,255,.3)" }}
-                        >
-                          <p className="fw-bold mb-0">Pablo Boa</p>
-                          <p className="text-light small mb-0">
-                            <MDBIcon far icon="clock" /> 12 mins ago
-                          </p>
-                        </MDBCardHeader>
-                        <MDBCardBody>
-                          <p className="mb-0">
-                            ohh mary! tú le apartastes los huesos a Digna, y la libra de carne.
-                          </p>
-                        </MDBCardBody>
-                      </MDBCard>
-                    </li>
-                    <li className="d-flex justify-content-between mb-4">
-                      <MDBCard className="w-100 mask-custom">
-                        <MDBCardHeader
-                          className="d-flex justify-content-between p-3"
-                          style={{ borderBottom: "1px solid rgba(255,255,255,.3)" }}
-                        >
-                          <p className="fw-bold mb-0">Mary Margarita</p>
-                          <p className="text-light small mb-0">
-                            <MDBIcon far icon="clock" /> 13 mins ago
-                          </p>
-                        </MDBCardHeader>
-                        <MDBCardBody>
-                          <p className="mb-0">
-                            ayy Pablo se me olvidó.
-                          </p>
-                        </MDBCardBody>
-                      </MDBCard>
-                      <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-5.webp"
-                        alt="avatar"
-                        className="rounded-circle d-flex align-self-start ms-3 shadow-1-strong"
-                        width="60"
-                      />
-                    </li>
-                    <li className="d-flex justify-content-between mb-4">
-                      <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp"
-                        alt="avatar"
-                        className="rounded-circle d-flex align-self-start me-3 shadow-1-strong"
-                        width="60"
-                      />
-                      <MDBCard className="mask-custom">
-                        <MDBCardHeader
-                          className="d-flex justify-content-between p-3"
-                          style={{ borderBottom: "1px solid rgba(255,255,255,.3)" }}
-                        >
-                          <p className="fw-bold mb-0">Pablo Boa</p>
-                          <p className="text-light small mb-0">
-                            <MDBIcon far icon="clock" /> 10 mins ago
-                          </p>
-                        </MDBCardHeader>
-                      </MDBCard>
-                    </li>
-                    <li className="mb-3">
-                    <MDBTextArea label="Message" name="mensaje" rows={4} onChange={ManejadorInput}/>
-                  </li>
-                  <MDBBtn color="light" size="lg" rounded className="float-end" onClick={enviarMensaje}>
-                    Send
-                  </MDBBtn>
-                  </MDBTypography>
-                </MDBCol>
+                {usuarioChat  ? (
+                  <MDBCol md="6" lg="7" xl="8" className="mask-custom">
+                    <MDBTypography listUnStyled className="text-white">
+                      <li className="d-flex justify-content-between mb-4">
+                        <img
+                          src={usuarioChat.perfil.imagen}
+                          alt="avatar"
+                          className="avatar1"
+                          width="60"
+                        />
+                        <MDBCard className="mask-custom">
+                          <MDBCardHeader
+                            className="d-flex justify-content-between p-3"
+                            style={{ borderBottom: "1px solid rgba(255,255,255,.3)" }}
+                          >
+                            <p className="fw-bold mb-0">{usuarioChat.nombre} {usuarioChat.apellidos}</p>
+                            <p className="text-light small mb-0">
+                              <MDBIcon far icon="clock" /> 12 mins ago
+                            </p>
+                          </MDBCardHeader>
+                          <MDBCardBody>
+                            <ul>
+                              {conversaciones.map(msm =>(
+                            <li style={{listStyleType: "none"}} key={msm.id_receptor.nombre}>
+                            <p className="mb-0">
+                             {msm.contenido}
+                            </p>
+                            </li>
+                            ))}
+                            </ul>
+                          </MDBCardBody>
+                        </MDBCard>
+                      </li>
+                      <li className="mb-3">
+                        <MDBTextArea label="Message" name="mensaje" value={mensajes.mensaje} rows={4} onChange={manejarInput}/>
+                      </li>
+                      <MDBBtn color="light" size="lg" rounded className="float-end" onClick={enviarMensaje}>
+                        Enviar
+                      </MDBBtn>
+                    </MDBTypography>
+                  </MDBCol>
+                ) : (
+                  <></>
+                )}
               </MDBRow>
             </MDBContainer>
       );
